a1a06910f7fd72a048eeb45c39da26b0d0c80bf9
Chaya Kalfon-Erentroy <57938697+chaya770@users.noreply.github.com>
2020-04-16 22:26:02 +0300

    Add files via upload

---
+++ Bill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Bill
+    {
+        public Bill(string d,int a,int p)
+        {
+            dishName = d;
+            amount = a;
+            price = p;
+                }
+        public string dishName { get; set; }
+        public int amount { get; set; }
+        public int price { get; set; }
+
+        public override string ToString()
+        { return this.ToStringProperty(); }
+
+    }
+}
